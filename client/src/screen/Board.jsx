bfed756ebea9a5311bb91a88f389d41ed08f4772
kanngji <alleneffort1994@gmail.com>
2022-11-10 01:01:40 +0900

    board 레이아웃 짜기

--- client/src/screen/Board.jsx
+++ client/src/screen/Board.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 export default function Board() {
-  return <div>Board</div>;
+  return (
+    <>
+      <div
+        style={{
+          width: "50%",
+          margin: "auto auto",
+          textAlign: "center",
+          border: "1px solid yellow",
+        }}
+      >
+        <h1>이것도 먹어보시지~</h1>
+        <Button
+          variant="outline-dark"
+          style={{ width: "100%" }}
+          onClick={() => Navigate("create")}
+        >
+          작성하기
+        </Button>
+      </div>
+      <div
+        className="container"
+        style={{ width: "90%", height: "90vh", border: "1px solid red" }}
+      >
+        asdsadfsdf
+      </div>
+      ;
+    </>
+  );
 }
--- client/src/screen/Diary.jsx
+++ client/src/screen/Diary.jsx
@@ -1,5 +1,18 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 export default function Diary() {
-  return <div>Diary</div>;
+  const navigate = useNavigate();
+  const [post, setPost] = useState({
+    title: "",
+    description: "",
+  });
+  return (
+    <>
+      <div style={{ width: "90%", margin: "auto auto", textAlign: "center" }}>
+        <h1>이런 순서로 개발이 되었어요!</h1>
+      </div>
+    </>
+  );
 }
