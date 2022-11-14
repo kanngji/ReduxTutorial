08642c7de8643cf4b465e2e56389770049391b6c
kanngji <alleneffort1994@gmail.com>
2022-11-14 21:24:18 +0900

    로그인 구현했는데 이게 맞나?

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,12 +3,25 @@ const bcrypt = require("bcryptjs");
 
 // login user
 const loginUser = async (req, res) => {
-  User.create({
-    email: req.body.email,
-    password: req.body.password,
-  })
-    .then((doc) => console.log(doc))
-    .catch((err) => console.log(err));
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    // console.log(user);
+    if (user) {
+      const validPass = await bcrypt.compare(password, user.password);
+      // console.log(validPass);
+      if (validPass) {
+        res.status(200).json("valid Email and pass! 로그인 완료");
+      } else {
+        res.json("wrong pass!");
+      }
+    } else {
+      res.status(404).json("user not found");
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Something broke!");
+  }
 };
 
 // register
